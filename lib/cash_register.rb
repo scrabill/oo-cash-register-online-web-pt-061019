6fce540d286d4ddf8a1a162c1e5d245f9f202d66
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-14 03:49:54 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :items, :last # Generic getter and setter
+
+  # @total = 0
+  # @discount = 0
+  #
+  # def initialize(total = 0, discount*)
+  #   @total = total
+  #   @discount = discount
+  #   # total = 0
+  # end
+
+  def initialize(discount = 0)
+    @total = 0 # And these assign them generic argument name
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += (price * quantity)
+    quantity.times do
+      @items << title
+    end
+    @last = (price * quantity)
+  end
+
+  def apply_discount
+    if @discount != 0
+      @total = @total.to_f * (1 - @discount.to_f / 100) # Turn the total into a float, add the discount, which is an integer, turned to a float
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    # @items.pop[-1]
+    @total -= @last
+  end
+
+end
